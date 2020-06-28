60c440afda0105fc3c62fc8eb70af11c71342cdf
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-06-28 21:43:45 +0530

    fix: minor issues related to code quality added doc comments, fixed naming issues, modified gitignore

--- src/main/java/com/imagelab/DashboardController.java
+++ src/main/java/com/imagelab/DashboardController.java
@@ -1,51 +1,71 @@
 package com.imagelab;
 
-import java.io.File;
-import java.io.IOException;
-import java.security.KeyStore;
-import java.util.List;
-
 import javafx.fxml.FXML;
 import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 
+import static utils.Constants.CIRCLE_FORMAT;
+
+/**
+ * The controller class of the dashboard
+ */
 public class DashboardController {
 
-    private final DataFormat circleFormat = new DataFormat("javafx.scene.shape.Circle");
+    @FXML
+    private AnchorPane buildAnchorPane;
 
     @FXML
-    private AnchorPane buidlanchorpane;
+    private Circle circleOperation;
 
+    /**
+     * To handle when users initial drag event.
+     * @param event Triggered mouse event.
+     *              When user tried to drag something that event
+     *              passed as a parameter.
+     */
     @FXML
-    private Circle circleoperation;
-
-    @FXML
-    private void handleDragDetection (MouseEvent event){
-        Dragboard db = circleoperation.startDragAndDrop(TransferMode.COPY);
-        db.setDragView(circleoperation.snapshot(null, null));
+    private void handleDragDetection (MouseEvent event) {
+        Dragboard dragboard = circleOperation.startDragAndDrop(TransferMode.COPY);
+        dragboard.setDragView(circleOperation.snapshot(null,null));
         ClipboardContent content = new ClipboardContent();
-        content.put(circleFormat, "circle");
-        db.setContent(content);
+        content.put(CIRCLE_FORMAT, "circle");
+        dragboard.setContent(content);
         System.out.println("Drag detected");
         event.consume();
     }
 
+    /**
+     * To handle when users initial drag event.
+     * @param event Triggered Drag event.
+     *              When user tried to drag something and
+     *              move over to another area
+     *              this function will be invoked.
+     */
     @FXML
-    private void handleDragOver (DragEvent event){
-        Dragboard db = event.getDragboard();
-        if (db.hasContent(circleFormat)) {
+    private void handleDragOver (DragEvent event) {
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasContent(CIRCLE_FORMAT)) {
             event.acceptTransferModes(TransferMode.COPY);
         }
+        System.out.println("Drag over detected");
     }
 
+    /**
+     * To handle when users initial drag event.
+     * @param event Triggered Drop event.
+     *              When user dropped the dragged
+     *              element this function will
+     *              trigger.
+     */
     @FXML
-    private void handleDrop(DragEvent event){
-        Dragboard db = event.getDragboard();
-        if (db.hasContent(circleFormat)) {
-            buidlanchorpane.getChildren().add(circleoperation);
+    private void handleDrop(DragEvent event) {
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasContent(CIRCLE_FORMAT)) {
+            buildAnchorPane.getChildren().add(circleOperation);
             event.setDropCompleted(true);
         }
+        System.out.println("Drop detected");
     }
-
 }
+
