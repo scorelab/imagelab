733564f0f562dd0cbd86bf7c5790ddbfb3e4d975
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-08-31 16:43:04 +0530

    Add: Junit5 configured + added one test + doc updated. This commit contains Junit 5 configurations and one unit test to make sure project tests are working. Running tests attached to maven lifecycle methods ('test') and documentation has been updated accordingly.

---
+++ src/test/java/RotateImageTest.java
@@ -0,0 +1,28 @@
+import com.imagelab.operator.OpenCVOperator;
+import com.imagelab.operator.basic.WriteImage;
+import com.imagelab.operator.geotransformation.RotateImage;
+import com.imagelab.operator.imagebluring.ApplyBlurEffect;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * RotateImage class related tests.
+ */
+class RotateImageTest {
+    /**
+     * To test whether validate method
+     * returns true if it receives an allowed operator
+     * and false for not allowed operators.
+     */
+    @Test
+    void validateShouldCheckWhetherOperatorIsAllowed() {
+        RotateImage rotateImage = new RotateImage();
+        OpenCVOperator writeImgOperator, blurImageOperator;
+        writeImgOperator = new WriteImage();
+        blurImageOperator = new ApplyBlurEffect();
+        //Test for allowed operators
+        Assert.assertTrue(rotateImage.validate(writeImgOperator));
+        //Test for not allowed operators.
+        Assert.assertFalse(rotateImage.validate(blurImageOperator));
+    }
+}
