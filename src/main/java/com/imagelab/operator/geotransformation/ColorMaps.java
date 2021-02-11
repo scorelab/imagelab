e5f835b09c7aded6f236ab29dbed9c3655d45fd5
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-02-12 01:38:06 +0530

    Add: ColorMaps to Geo Transformations + Dashboard + Styles

---
+++ src/main/java/com/imagelab/operator/geotransformation/ColorMaps.java
@@ -0,0 +1,102 @@
+package com.imagelab.operator.geotransformation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import com.imagelab.operator.OpenCVOperator;
+import com.imagelab.operator.basic.ReadImage;
+import com.imagelab.operator.basic.WriteImage;
+import com.imagelab.operator.filtering.ApplyBoxFilter;
+import com.imagelab.operator.filtering.ApplyDilation;
+import com.imagelab.operator.filtering.ApplyErosion;
+import com.imagelab.operator.imagebluring.ApplyBlurEffect;
+import com.imagelab.operator.imagebluring.ApplyGaussianBlurEffect;
+import com.imagelab.operator.imagebluring.ApplyMedianBlurEffect;
+import com.imagelab.operator.imageconversion.ColoredImageToBinary;
+import com.imagelab.operator.imageconversion.ConvertToGrayscale;
+
+public class ColorMaps extends OpenCVOperator{
+
+	
+	/**
+     * To get the type of color map.
+     *
+     * @return - output type with number int.
+     */
+	private int type = 1;
+	
+	
+	@Override
+	public boolean validate(OpenCVOperator previous) {
+		if (previous == null) {
+            return false;
+        }
+        return allowedOperators().contains(previous.getClass());
+	}
+
+	@Override
+	public Mat compute(Mat image) {
+		// call to the private function
+		return applyColorMaps(image, getType());
+	}
+	
+	private Mat applyColorMaps(Mat image,int type) {
+		
+		Mat dst = new Mat();
+		//Color map to applying to the image
+		switch(type) {
+		case 1 : Imgproc.applyColorMap(image, dst, Imgproc.COLORMAP_HOT);break;
+		case 2 : Imgproc.applyColorMap(image, dst, Imgproc.COLORMAP_AUTUMN);break;
+		case 3 : Imgproc.applyColorMap(image, dst, Imgproc.COLORMAP_BONE);break;
+		case 4 : Imgproc.applyColorMap(image, dst, Imgproc.COLORMAP_COOL);break;
+		case 5 : Imgproc.applyColorMap(image, dst, Imgproc.COLORMAP_RAINBOW);break;
+		}
+//		Imgproc.applyColorMap(image, dst, Imgproc.COLORMAP_BONE);
+		return dst;
+	}
+
+	@Override
+	public Set<Class<?>> allowedOperators() {
+		Set<Class<?>> allowed = new HashSet<>();
+		allowed.add(ReadImage.class);
+        allowed.add(RotateImage.class);
+        allowed.add(WriteImage.class);
+        allowed.add(ColoredImageToBinary.class);
+        allowed.add(ConvertToGrayscale.class);
+        allowed.add(ApplyBlurEffect.class);
+        allowed.add(ApplyGaussianBlurEffect.class);
+        allowed.add(ApplyMedianBlurEffect.class);
+        allowed.add(ApplyBoxFilter.class);
+        allowed.add(ApplyDilation.class);
+        allowed.add(ApplyErosion.class);
+        allowed.add(ColorMaps.class);
+        return allowed;
+		
+	}
+	public int getType() {
+		return type;
+	}
+	/**
+     * To set the dilation iteration of the output image.
+     *
+     * @return - output depth.
+     */ 
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public enum Information{
+		OPERATOR_INFO{
+			public String toString() {
+				return "Color Maps\n\n Colormaps can apply different color maps to an image using this method."
+						+" OpenCV caters various other types"
+						+" All these types are represents by predefined static fields(fixed values)";
+						
+			}
+		}
+	}
+	
+}
---
+++ src/main/java/com/imagelab/view/forms/ColorMapsPropertiesForm.java
@@ -0,0 +1,94 @@
+package com.imagelab.view.forms;
+
+import com.imagelab.operator.geotransformation.ColorMaps;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.VBox;
+
+public class ColorMapsPropertiesForm extends AbstractPropertiesForm{
+	public ColorMapsPropertiesForm(ColorMaps operator) {
+		//Color Maps title container.
+        PropertiesFormTitleContainer ColorMapsTitleContainer;
+        ColorMapsTitleContainer = new PropertiesFormTitleContainer("Color Maps Properties");
+        
+        
+        // color map container
+        VBox colorContainer = new VBox();
+        colorContainer.setPrefWidth(205.0);
+        colorContainer.setSpacing(10);
+        //Different ColorMaps Choices.
+        ToggleGroup color_maps = new ToggleGroup();
+        
+        RadioButton r1 = new RadioButton("HOT");
+        RadioButton r2 = new RadioButton("AUTUMN");
+        RadioButton r3 = new RadioButton("BONE");
+        RadioButton r4 = new RadioButton("COOL");
+        RadioButton r5 = new RadioButton("HSV");
+        RadioButton r6 = new RadioButton("JET");
+        RadioButton r7 = new RadioButton("OCEAN");
+        RadioButton r8 = new RadioButton("PARULA");
+        RadioButton r9 = new RadioButton("PINK");
+        RadioButton r10 = new RadioButton("RAINBOW");
+        
+        r1.setToggleGroup(color_maps);
+        r2.setToggleGroup(color_maps);
+        r3.setToggleGroup(color_maps);
+        r4.setToggleGroup(color_maps);
+        r5.setToggleGroup(color_maps);
+        r6.setToggleGroup(color_maps);
+        r7.setToggleGroup(color_maps);
+        r8.setToggleGroup(color_maps);
+        r9.setToggleGroup(color_maps);
+        r10.setToggleGroup(color_maps);
+        
+        // color map container1
+        VBox colorContainer1 = new VBox(r1,r2,r3,r4,r5,r6,r7,r8,r9,r10);
+        colorContainer1.setPrefWidth(205.0);
+        colorContainer1.setSpacing(7);
+        
+        r3.setSelected(true);
+        
+        
+        
+        color_maps.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+        	RadioButton selectedRadioButton =
+                    (RadioButton) newValue;
+            
+            selectedRadioButton.setSelected(true);
+            String toggleGroupValue = selectedRadioButton.getText();
+            // Pass the value to Controller
+            switch(toggleGroupValue) {
+            	case "HOT" : operator.setType(1);break;
+            	case "AUTUMN" : operator.setType(2);break;
+            	case "BONE" : operator.setType(3);break;
+            	case "COOL" : operator.setType(4);break;
+            	case "RAINBOW" : operator.setType(5);break;
+            }
+        });
+                
+        //Space container
+        VBox spaceContainer = new VBox();
+        spaceContainer.setPrefWidth(205.0);
+        spaceContainer.setSpacing(10);
+        Label lblSpaceOne = new Label("");
+        Label lblSpaceTwo = new Label("");
+        spaceContainer.getChildren().addAll(lblSpaceOne, lblSpaceTwo);
+        
+        VBox colorMapsPropertiesContainer = new VBox();
+        colorMapsPropertiesContainer.setPrefSize(205, 47);
+        colorMapsPropertiesContainer.setSpacing(20);
+        colorMapsPropertiesContainer.setLayoutX(14);
+        colorMapsPropertiesContainer.setLayoutY(14);
+        colorMapsPropertiesContainer.getChildren().addAll(
+        		ColorMapsTitleContainer,
+        		colorContainer,
+        		colorContainer1,
+        		spaceContainer
+               
+        );
+        getChildren().addAll(colorMapsPropertiesContainer);
+	}
+}
