1af8f4d296126d7e88449345bbd68799e9bee4a9
Pola Hano Shehat <43797542+polahano@users.noreply.github.com>
2021-08-06 22:44:50 +0200

    Adding guide tour

---
+++ electron_app/guide.js
@@ -0,0 +1,71 @@
+let tour = introJs();
+
+tour.setOptions({
+    steps: [{
+        title: 'Welcome',
+        intro: '<img src="./assets/logos/Image Lab full logo.png" height="80px"> <p style="line-height=20px">Welcome to Image lab! 👋 \n Let\'s recognize our interface step by step...</p>',
+    },
+    {
+        element: document.querySelector('.playground-pane'),
+        title: 'Playground',
+        intro: 'This  is the workspace where you can use blocksto do the process youu want to do on your picture.',
+        position: 'right',
+    },
+    {
+        element: document.querySelector('#blockly-0.blocklyTreeRow'),
+        title: 'Category',
+        intro: 'On clicking any category, a bunch of blocks of operations related to this category appears. \n Then, you can drag blocks and drop it into workspace.',
+        position: 'left',
+        disableInteraction: false,
+    },
+    {
+        element: document.querySelector('.preview-pane'),
+        title: 'Preview area',
+        intro: 'This is the area where your processed image is shown',
+        position: 'bottom',
+    },
+    {
+        element: document.querySelector('.properties-pane'),
+        title: 'Properties area',
+        intro: 'Here, you can modify properies of the block of operation you choose.',
+        position: 'top',
+    },
+    {
+        element: document.querySelector('.information-pane'),
+        title: 'Block definition',
+        intro: 'This part shows a brief about the operation of the block you choose.',
+        position: 'top',
+    },
+    {
+        title: 'Enjoy!',
+        intro: 'Let\'s do great things!',
+    },
+    ],
+});
+
+// tour.setOptions({
+//     hints: [
+//         {
+//             hint: 'text for the hint',
+//             element: document.querySelector('.properties-pane'),
+//             position: 'left',
+//         },
+//         {
+//             hint: 'These are categories',
+//             element: document.querySelector('#blockly-0.blocklyTreeRow'),
+//             hintPosition: 'middle-right',
+//         },
+//         {
+//             hint: 'text for the hint',
+//             element: document.querySelector('.blocklyToolboxCategory'),
+//         },
+//         {
+//             hint: 'text for the hint',
+//             element: document.querySelector('.blocklyToolboxContents'),
+//         },
+//     ]
+// });
+
+
+tour.showHints()
+tour.start();
