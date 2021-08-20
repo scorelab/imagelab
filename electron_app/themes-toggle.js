bba886fcbc6b21cef9f74a99526fcbaa4afde852
Pola Hano Shehat <43797542+polahano@users.noreply.github.com>
2021-08-20 17:39:46 +0200

    commenting and removing unused lines

--- electron_app/themes-toggle.js
+++ electron_app/themes-toggle.js
@@ -1,11 +1,8 @@
+// This block of code is responsible for toggling between light/dark modes
 document.querySelector('.toggle').addEventListener('click', function () {
-    this.classList.add('animate');
-
     setTimeout(() => {
         this.classList.toggle('active');
         document.querySelector('.wave').classList.toggle('active');
         document.documentElement.classList.toggle('dark-mode');
     }, 150);
-
-    setTimeout(() => this.classList.remove('animate'), 300);
 });
