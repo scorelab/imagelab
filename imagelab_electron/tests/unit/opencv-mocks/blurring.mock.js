8ad18862eccc72ca8397a42a805a1f775facda08
Abdullah Ranginwala <abdullahranginwala20@gmail.com>
2023-08-10 09:04:18 +0530

    added test coverage for Gaussian and Median blur

--- imagelab_electron/tests/unit/opencv-mocks/blurring.mock.js
+++ imagelab_electron/tests/unit/opencv-mocks/blurring.mock.js
@@ -1,9 +1,11 @@
 module.exports = {
     cv2: {
         Mat: jest.fn(),
-        Size: jest.fn((width, height) => ({ width, height })), // Mock Size with a function that returns an object
-        Point: jest.fn((x, y) => ({ x, y })), // Mock Point with a function that returns an object
+        Size: jest.fn((width, height) => ({ width, height })),
+        Point: jest.fn((x, y) => ({ x, y })), 
         blur: jest.fn(),
+        GaussianBlur: jest.fn(),
+        medianBlur: jest.fn(),
     },
   };
   
