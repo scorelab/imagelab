3343286ea7c515b8386ff6baef80a08880950ace
Abdullah Ranginwala <abdullahranginwala20@gmail.com>
2023-08-17 20:34:01 +0530

    added bilateral and box filter unit testing

---
+++ imagelab_electron/tests/unit/filtering/BilateralFilter.test.js
@@ -0,0 +1,31 @@
+const BilateralFilter = require('../../../src/operator/filtering/BilateralFilter')
+const opencvMock = require('../opencv-mocks/filtering.mock');
+
+describe('BilateralFilter Operator', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should apply bilateral filter with given parameters', () => {
+    const operator = new BilateralFilter('type', 'id');
+    operator.cv2 = opencvMock.cv2;
+
+    const imageMock = 'mockImageData';
+
+    operator.setParams('filterSize', 5); // Set filter size
+    operator.setParams('sigmaColor', 75); // Set sigma color
+    operator.setParams('sigmaSpace', 75); // Set sigma space
+
+    operator.compute(imageMock);
+
+    expect(opencvMock.cv2.Mat).toHaveBeenCalledTimes(1);
+    expect(opencvMock.cv2.bilateralFilter).toHaveBeenCalledWith(
+      imageMock,
+      {},
+      5,
+      75,
+      75,
+      opencvMock.cv2.BORDER_DEFAULT
+    );
+  });
+});
